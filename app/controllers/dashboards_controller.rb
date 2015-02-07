e974ef99d7723414ef44b05b9ecd38e1079e870d
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-07 10:31:20 -0800

    Cleaned up visual of forms with tabbing.  Able to edit newsfeed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -12,12 +12,31 @@ class DashboardsController < ApplicationController
 
   def create
     @newsfeed = Newsfeed.create(content:params[:newsfeed][:content], admin:current_admin, school:current_school)
+    if @newsfeed.save
+      flash[:notice] = 'Newsfeed added!'
+    else
+      flash[:errors] = @newsfeed.errors.full_messages
+    end
+    redirect_to dashboard_path @newsfeed
+  end
+
+  def show
+    @newsfeed = Newsfeed.find(params[:id])
   end
 
   def edit
+    @newsfeed = Newsfeed.find(params[:id])
   end
 
   def update
+     @newsfeed = Newsfeed.find(params[:id])
+    if @newsfeed.update(content:params[:newsfeed][:content], admin:current_admin, school:current_school)
+      redirect_to dashboard_path @newsfeed
+    else
+      flash[:errors] = @newsfeed.errors.full_messages
+      puts @newsfeed.errors.full_messages
+      redirect_to edit_dashboard_path @newsfeed
+    end
   end
 
   def destroy
--- app/models/newsfeed.rb
+++ app/models/newsfeed.rb
@@ -1,4 +1,6 @@
 class Newsfeed < ActiveRecord::Base
   belongs_to :school
   belongs_to :admin
+
+  validates :content, :admin, :school, presence: true
 end
