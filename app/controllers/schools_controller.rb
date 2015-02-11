4a7882b1e387f8977de8a801fb4cb5334e77a358
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-10 16:04:27 -0800

    Cleaned up weather api link and removed if

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -10,13 +10,7 @@ class SchoolsController < ApplicationController
   	@id = params[:id].to_i
   	@newsfeed = Newsfeed.where(school:params[:id]).reverse
 
-	$weather_uri = "https://api.worldweatheronline.com/free/v1/weather.ashx?key=jtpc4myth9fwxjgwz9fh5fw5&q="
-	if @id == 1 	
-  		$weather_uri << "32836&fx=no&format=json"
-  	elsif @id == 2
-  		$weather_uri << "84720&fx=no&format=json"
-  	end
-  	puts $weather_uri
+	  $weather_uri = "https://api.worldweatheronline.com/free/v1/weather.ashx?key=jtpc4myth9fwxjgwz9fh5fw5&q=#{@school.zipcode}&fx=no&format=json"
 
   	uri = URI.parse($weather_uri)
   	http = Net::HTTP.new(uri.host, uri.port)
